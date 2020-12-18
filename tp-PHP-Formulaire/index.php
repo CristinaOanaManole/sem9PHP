fe66381912ea677dc72a479fd6223eb412feabc0
CristinaOanaManole <oana_manole@yahoo.com>
2020-12-18 23:46:15 +0100

    modif galerie

--- tp-PHP-Formulaire/index.php
+++ tp-PHP-Formulaire/index.php
@@ -1,36 +1,37 @@
+<?php
+$directory = 'img/';
+$files = scandir($directory);
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/style.css">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <title>PHP - Upload d'images</title>
 </head>
 
 <body>
-<div class="container bg-light text-center mt-5">
-    <!--<h1># PHP - Upload d'images</h1>-->
-    <div class="form">
-        <form action="index.php" method="post" enctype="multipart/form-data">
-            <label for="fileToUpload">Veuillez choisir une image:</label>
-            <!-- <input type="hidden" name="MAX_FILE_SIZE" value="1048576"> -->
-            <input type="file" name="fileToUpload" id="fileToUpload">
-            <input type="submit" value="Valider">
-        </form>
-        <img id="imgPreview">
-    </div>
+    <div class="container bg-light text-center mt-5">
+        <h1># PHP - Upload d'images</h1>
+        <div class="form">
+            <form action="index.php" method="post" enctype="multipart/form-data">
+                <label for="fileToUpload">Veuillez choisir une image:</label>
+                <!-- <input type="hidden" name="MAX_FILE_SIZE" value="1048576"> -->
+                <input type="file" name="fileToUpload" id="fileToUpload" accept="image/*">
+                <input type="submit" value="Valider">
+            </form>
+            <img id="imgPreview">
+            <a href="galerie.php"><button type="button" class="btn btn-primary m-2" action="galerie.php">
+                    Notifications <span class="badge bg-secondary"><?= count($files) - 2 ?></span>
+                </button></a>
+        </div>
 
-    <!-- $_FILES['fileToUpload']['size'] -->
-    <!-- Taille maxi: 1Mo soit 1048576 octets -->
-    <!-- var_dump(pathinfo($_FILES['fileToUpload']['name']));
-    $_FILES['fileToUpload']['tmp_name'] 
-    -->
-
-   <!-- Taille maxi: 1Mo soit 1048576 octets soit 1024 * 1024 -->
+        <!-- Taille maxi: 1Mo soit 1048576 octets soit 1024 * 1024 -->
         <!-- 1024 = 1 Ko // 1024 * 2 = 2ko... // 1024 * 1024 = 1 Mo // 1024 * 1024 *1024 = 1 Go // ... -->
 
         <?php
@@ -46,7 +47,7 @@
             }
         }
         ?>
-</div>
+    </div>
  
 <!-- Option 1: Bootstrap Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
