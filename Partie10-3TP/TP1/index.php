56d9928f9c4204b3df740ec1098be88cc6adc4dc
CristinaOanaManole <oana_manole@yahoo.com>
2021-01-06 22:01:23 +0100

    manque centrage

--- Partie10-3TP/TP1/index.php
+++ Partie10-3TP/TP1/index.php
@@ -17,7 +17,7 @@
 <body>
     <div class="container mt-5">
         <div class="row">
-            <div class="col-lg-6 col-md-8 col-sm-10 col-12 mx-auto">
+            <div class="col-lg-8 col-md-10 col-sm-12 col-12 mx-auto">
                 <div class="card login-content shadow-lg border-0">
                     <h1 class="text-center m-5 pt-5">Formulaire d'inscription</h1>
 
@@ -26,15 +26,15 @@
                                 Les champs marqué par </span>
                             <em>*</em> sont <em> obligatoires </em>
                         </p>
-                    
-                        <div class="">
-                        <fieldset>
-                            <legend class="text-center m-2">Contact</legend>
 
-                                <div class="text-left">
-                                   
-                                    <label for="lastName">Votre nom : <em>*</em></label>
-                                    <input type="text" name="lastName" id="lastName" placeholder="Indiquer votre nom" required="" minlength="4" maxlength="20" size="20" value="<?php echo !empty($lastName) ? $lastName : ''; ?>">
+                        <div class="mx-auto">
+                            <fieldset>
+                                <legend class="text-center mb-2 mt-2">Contact</legend>
+
+                                <div class="px-4 m-1">
+
+                                    <label for="lastName" class="px-2">Votre nom : <em>*</em></label>
+                                    <input class="m-3" type="text" name="lastName" id="lastName" placeholder="Indiquer votre nom" required="" minlength="4" maxlength="20" size="20" value="<?php echo !empty($lastName) ? $lastName : ''; ?>">
                                     <?php if (!empty($lastNameError)) : ?>>
                                     <!--on vérifie sil y a une erreur-->
 
@@ -43,79 +43,78 @@
 
                                 <?php endif; ?>
 
-                                </div>
-                                    
+                                <label for="firstName" class="px-2">Votre prénom :<em>*</em></label>
+                                <input type="text" name="firstName" id="firstName" placeholder="Indiquer votre prénom" required="" minlength="4" maxlength="20" size="20" value="<?php echo !empty($firstName) ? $firstName : ''; ?>">
+                                <?php if (!empty($firstNameError)) : ?>
+                                    <!--on vérifie sil y a une erreur-->>
 
-                                <div class="text-left">
-                                    <label for="firstName">Votre prénom :<em>*</em></label>
-                                    <input type="text" name="firstName" id="firstName" placeholder="Indiquer votre prénom" required="" minlength="4" maxlength="20" size="20" value="<?php echo !empty($firstName) ? $firstName : ''; ?>">
-                                    <?php if (!empty($firstNameError)) : ?>
-                                        <!--on vérifie sil y a une erreur-->>
+                                    <p> <span class="red"><?php echo $firstNameError; ?></span></p>
+                                    <!--dans ce cas on l'affiche-->
 
-                                        <p> <span class="red"><?php echo $firstNameError; ?></span></p>
-                                        <!--dans ce cas on l'affiche-->
-
-                                    <?php endif; ?>
+                                <?php endif; ?>
                                 </div>
 
-                                <div class="text-left">
-                                    <label for="address">Votre adresse :<em>*</em></label>
+                                <div class="px-4 m-1">
+                                    <label for="address" class="px-2">Votre adresse :<em>*</em></label>
                                     <input type="text" id="address" name="address" placeholder="1 rue Colbert 75000 Paris" required>
-                                </div>
 
-                                <div class="text-left">
-                                    <label for="phone">Votre téléphone :<em>*</em></label>
+
+                                    <label for="phone" class="px-2">Votre téléphone :<em>*</em></label>
                                     <input type="text" id="phone" name="phone" placeholder="0612345678" required>
                                 </div>
-                                    </fieldset>
+                            </fieldset>
 
-                                <fieldset>
-                                    <legend class="text-center m-2">Informations personnelles</legend>
-                                    <label for="dateofBirth">Votre date de naissance<em>*</em></label>
-                                    <input id="dateofBirth" type="date" placeholder="jour/mois/année" pattern="[0-9]{1-2}" required=""><br> <!--  -->
+                            <fieldset>
+                                <legend class="text-center mb-2 mt-2">Informations personnelles</legend>
+                                <div class="container">
+                                    <div class="">
+                                        <label for="dateofBirth" class="">Votre date de naissance<em>*</em></label>
+                                        <input id="dateofBirth" type="date" placeholder="jour/mois/année" pattern="[0-9]{1-2}" required=""><br> <!--  -->
 
-                                    <div class="text-left">
-                                        <label for="birthCountry">Pays de naissance :</label>
-                                        <input type="text" id="birthCountry" name="birthCountry">
-                                    </div>
+                                        <div class="">
+                                            <label for="birthCountry" class="">Pays de naissance :</label>
+                                            <input type="text" id="birthCountry" name="birthCountry">
+                                        </div>
 
-                                    <div class="text-left">
-                                        <label for="countryOrigin">Nationalité :</label>
-                                        <input type="text" id="countryOrigin" name="countryOrigin">
-                                    </div>
+                                        <div class="">
+                                            <label for="countryOrigin">Nationalité :</label>
+                                            <input type="text" id="countryOrigin" name="countryOrigin">
+                                        </div>
 
-                                    <div class="text-left">
-                                        <label for="email">Adresse mail :<em>*</em></label>
-                                        <input name="email" type="text" size="22">
-                                    </div>
+                                        <div class="">
+                                            <label for="email">Adresse mail :<em>*</em></label>
+                                            <input name="email" type="text" size="22">
+                                        </div>
 
-                                    <div class="text-left">
-                                        <label for="lien">Liens codecademy :</label>
-                                        <input type="text" name="url" size="12" value="http://">
-                                    </div>
+                                        <div class="">
+                                            <label for="lien">Liens codecademy :</label>
+                                            <input type="text" name="url" size="12" value="http://">
+                                        </div>
 
-                                    <div class="text-left">
-                                        <label for="comments">Pourquoi voulez-vous vous impliquer dans l'organisation du point γ?</label>
-                                        <textarea id="comments"></textarea>
-                                    </div>
-                                </fieldset>
+                                        <div class="">
+                                            <label for="comments">Pourquoi voulez-vous vous impliquer dans l'organisation du point γ?</label>
+                                            <textarea id="comments"></textarea>
+                                        </div>
+                            </fieldset>
 
-                                <select name="graduate" size="1">
-                                    <radio name="sans BAC" value="sans BAC">sans BAC
-                                        <radio name="BAC" value="BAC">BAC
-                                </select>
-                                <radio>Avez vous déjà eu une expérience avec la programmation et/ou l'informatique avant de remplir ce formulaire ?</radio>
-                                <radio></radio>
-                                <input type="submit" name="submit" value="valider"></p>
-                    </form>
-
+                            <select name="graduate" size="1">
+                                <radio name="sans BAC" value="sans BAC">sans BAC
+                                    <radio name="BAC" value="BAC">BAC
+                            </select>
+                            <radio>Avez vous déjà eu une expérience avec la programmation et/ou l'informatique avant de remplir ce formulaire ?</radio>
+                            <radio></radio>
+                            <input type="submit" name="submit" value="valider"></p>
+                        </div>
                 </div>
+                </form>
+
             </div>
         </div>
     </div>
-    
-    
-    
+    </div>
+
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
