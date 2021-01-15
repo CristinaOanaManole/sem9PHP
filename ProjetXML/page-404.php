59e003e4bb75a8620291d5dbfc67f77a1b655c09
CristinaOanaManole <oana_manole@yahoo.com>
2021-01-15 11:12:17 +0100

    css en cours

---
+++ ProjetXML/page-404.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title></title>
+<link rel="stylesheet" href="assets/style.css">
+</head>
+<body>  
+
+<nav class="navbar navbar-expand-lg navbar-light menu">
+  <a class="navbar-brand " href="http://sem9php/ProjetXML/accueil.html">Maçonnerie Ocordo</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="http://sem9php/ProjetXML/accueil.html">Accueil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://sem9php/ProjetXML/aboutUs.html">Qui sommes nous ?</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://sem9php/ProjetXML/opinion.html">Nos clients témoignent</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://sem9php/ProjetXML/contact.html">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container text-center">
+    <h1>Nous n'avons pas trouvé la page.</h1>
+    <img src="assets/img/bebe-ballon.jpg" alt="Page inexistante">
+</div>
+
+<footer class="bg-light text-center align-items-center justify-content-center p-4">
+        <p class="text-center">Maçonnerie Ocordo © 2021 Tous les droits réserves </p>
+    </footer>
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>  
+</body>
+</html>
--- README.md
+++
@@ -1,6 +0,0 @@
-# sem9PHP
-# Exercises PHP as part of the training
-Forms
-Date
-Cookies
-Sessions
